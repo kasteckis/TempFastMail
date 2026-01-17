84b2213a770b2f9930000516b67163ade868ce82
Valentinas <plankton546@gmail.com>
2026-01-17 20:07:53 +0200

    Add recent blog posts section to homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\BlogRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
+    public function __construct(
+        private BlogRepository $blogRepository,
+    ) {
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'blogs' => $this->blogRepository->findBy([], ['createdAt' => 'DESC'], 4),
+        ]);
     }
 }
